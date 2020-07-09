0fb4f03cc3b152191f1bf525268c68c264560cfc
reda <reda.aazmi@gmail.com>
2020-07-09 10:55:40 +0200

    package name change

--- app/src/androidTest/java/com/theappexperts/supergit/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/theappexperts/supergit/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.javabobo.supergit", appContext.packageName)
+        assertEquals("com.theappexperts.supergit", appContext.packageName)
     }
 }
